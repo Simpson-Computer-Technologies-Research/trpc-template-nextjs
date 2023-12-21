60c4fc05738ed6aaa9f2f080163f7bde16a244e4
Tristan <heytristaann@gmail.com>
2023-12-20 22:04:18 -0400

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,17 +7,11 @@ const config = {
   plugins: ["@typescript-eslint"],
   extends: [
     "plugin:@next/next/recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
